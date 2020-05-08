f5941d4d9ef7ccd1a9fab0131cb35ed2639a3068
unknown <omarzero130@gmail.com>
2020-05-09 00:40:48 +0200

    first commit

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../screens/category_meals.dart';
+ 
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+  CategoryItem(
+    this.id,
+    this.title,
+    this.color,
+  );
+  void selectCat(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(CategoryMealsScreen.routeName, arguments: {
+      'id': id,
+      'title': title,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCat(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomCenter,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
